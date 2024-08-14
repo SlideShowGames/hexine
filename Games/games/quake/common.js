f4ed2614d218fa3032ee9b38f9fef23971410690
Bradley Stratton <xboxps406@icloud.com>
2024-08-14 08:59:54 -0400

    this should fix quake

--- Games/games/quake/common.js
+++
@@ -1,9 +0,0 @@
-
-function deleteServerSockets() {
-	chrome.sockets.tcpServer.getSockets(function(infos) {
-		infos.forEach( function(info) {
-			chrome.sockets.tcpServer.close(info.socketId,
-										   function(i) { console.log('closed server socket',i) })
-		})
-	})
-}
--- Games/games/quake/package.sh
+++
@@ -1,9 +0,0 @@
-rm package.zip
-
-zip package.zip -r manifest.json *.png *.js *.html WebQuake-async/Client server
-
-#zip package.zip -r manifest.json *.png *.js *.html WebQuake/Client web-server-chrome/*.js
-
-#zip package.zip -r * -x package.sh -x *.git* -x "*.*~" -x web-server-chrome/manifest.json -x web-server-chrome/*.png -x WebQuake/Server/* WebQuake/Server/id1/*
-
-#web-server-chrome/*.js *.html *.js manifest.json WebQuake/* WebQuake/Client/* WebQuake/Client/id1/* WebQuake/Client/WebQuake/* *.png
